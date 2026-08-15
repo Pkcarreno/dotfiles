5c2c322592b7f8f282217f2cb92bf785b4a96270
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-08-14 22:30:32 -0400

    fix: remove default mason config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,4 @@
 return {
-  -- tools
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
-      })
-    end,
-  },
-
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
