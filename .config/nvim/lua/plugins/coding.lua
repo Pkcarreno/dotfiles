ae20da6dfd19e2c3cd6316be695b84b82fcc4521
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-08-14 22:25:50 -0400

    chore: remove unused plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,20 +1,4 @@
 return {
-  -- Go forward/backward with square brackets
-  {
-    "nvim-mini/mini.bracketed",
-    event = "BufReadPost",
-    config = function()
-      local bracketed = require("mini.bracketed")
-      bracketed.setup({
-        file = { suffix = "" },
-        window = { suffix = "" },
-        quickfix = { suffix = "" },
-        yank = { suffix = "" },
-        treesitter = { suffix = "n" },
-      })
-    end,
-  },
-
   {
     "saghen/blink.cmp",
     opts = {
