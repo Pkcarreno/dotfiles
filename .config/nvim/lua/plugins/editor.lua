48d92a5014f5bbcfb93fefb955b2cea8decca907
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-08-14 22:27:13 -0400

    chore: remove unused plugin

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -55,15 +55,4 @@ return {
       },
     },
   },
-
-  {
-    "vuki656/package-info.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      package_manager = "pnpm",
-      hide_up_to_date = true,
-    },
-  },
 }
