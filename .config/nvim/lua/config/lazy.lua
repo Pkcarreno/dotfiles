f873f38b1a497b8938a478c8205872faf41f52b6
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-08-14 21:57:34 -0400

    feat: set kanagawa as colorscheme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "mayu",
+        colorscheme = "kanagawa",
         news = {
           lazyvim = false,
           neovim = false,
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,59 @@
 return {
   {
-    "myypo/borrowed.nvim",
-    lazy = true,
+    "rebelot/kanagawa.nvim",
+    lazy = false,
     priority = 1000,
     opts = {
+      compile = false,
       transparent = true,
-      overrides = {
-        groups = {
+      dimInactive = true,
+      colors = {
+        theme = {
           all = {
-            DashboardShortCut = { link = "keyword" },
-            DashboardHeader = { fg = "speak" },
-            DashboardCenter = { link = "comment" },
-            DashboardFooter = { fg = "extra", style = "italic" },
-            InclineNormal = { bg = "speak", fg = "sheet" },
-            LspReferenceText = { bg = "blanket" },
-            LspReferenceWrite = { bg = "blanket" },
-            LspReferenceRead = { bg = "blanket" },
+            ui = {
+              bg_gutter = "none",
+            },
           },
         },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        local makeDiagnosticColor = function(color)
+          local c = require("kanagawa.lib.color")
+          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
+
+        return {
+          -- dark completion menu
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+
+          -- tint background of diagnostics messages
+          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+
+          -- transparent flaoting window background
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        }
+      end,
+      -- theme = "dragon",
+      background = {
+        dark = "dragon",
+        light = "lotus",
       },
     },
   },
