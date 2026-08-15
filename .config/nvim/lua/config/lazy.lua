b3ef61cd60cd1903cb82f0b64cdbecc7f7331ba7
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-08-15 15:20:03 -0400

    feat: migrate custom keymaps to yanky.nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ai.supermaven" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
