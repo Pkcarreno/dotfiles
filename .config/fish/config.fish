c3bae943cb585a71a1c6e7a3e700c60694318f3f
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-08-18 11:27:54 -0400

    refactor(fish): make use of status is-interactive for user-facing settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,3 @@
-set fish_greeting ""
-
-# theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user no
-set -g theme_hide_hostname yes
-set -g theme_hostname no
-
-# aliases
-alias ls "ls -p -G"
-alias la "ls -A"
-alias ll "ls -l"
-alias lla "ll -A"
-alias g git
-command -qv nvim && alias vim nvim
-
 set -gx EDITOR nvim
 
 set -gx PATH bin $PATH
@@ -25,23 +8,10 @@ set -gx PATH ~/.local/bin $PATH
 set -g GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
 
-if type -q eza
-    alias ll "eza -l -g --icons"
-    alias lla "ll -a"
-end
-
-if type -q tmux
-    alias ide "~/.scripts/ide"
-end
-
 if type -q /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     set -Ux HOMEBREW_NO_AUTO_UPDATE 1
 end
-
-# Fzf
-set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
-set -g FZF_LEGACY_KEYBINDINGS 0
 
 switch (uname)
     case Darwin
@@ -53,6 +23,36 @@ switch (uname)
 end
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
-if test -f $LOCAL_CONFIG
-    source $LOCAL_CONFIG
+test -f $LOCAL_CONFIG; and source $LOCAL_CONFIG
+
+if status is-interactive
+    set fish_greeting ""
+
+    # theme
+    set -g theme_color_scheme terminal-dark
+    set -g fish_prompt_pwd_dir_length 1
+    set -g theme_display_user no
+    set -g theme_hide_hostname yes
+    set -g theme_hostname no
+
+    # Fzf
+    set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
+    set -g FZF_LEGACY_KEYBINDINGS 0
+
+    # aliases
+    alias ls "ls -p -G"
+    alias la "ls -A"
+    alias ll "ls -l"
+    alias lla "ll -A"
+    alias g git
+    command -qv nvim && alias vim nvim
+
+    if type -q eza
+        alias ll "eza -l -g --icons"
+        alias lla "ll -a"
+    end
+
+    if type -q tmux
+        alias ide "~/.scripts/ide"
+    end
 end
